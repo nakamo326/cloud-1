19220686888a504ec59d5132136b28c0e9bc03ff
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-03-20 22:09:40 +0900

    albのエグレスを許可

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -55,14 +55,27 @@ resource "aws_alb_target_group_attachment" "alb_target_group_attachment" {
 
 resource "aws_security_group" "alb_sg" {
   name        = "cloud-1-alb-sg"
-  description = "Allow HTTP"
+  description = "Allow HTTP and HTTPS traffic"
   vpc_id      = aws_vpc.vpc.id
 
-  # HTTP
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
