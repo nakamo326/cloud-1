025854f335d7cf27c73471bf99922f8138864a13
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-03-21 23:47:20 +0900

    albにinternet gatewayが必要だったので復帰

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -24,11 +24,24 @@ resource "aws_subnet" "dummy_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "cloud-1-igw"
+  }
+}
+
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.vpc.id
   tags = {
     Name = "cloud-1-route-table"
   }
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "subnet_association" {
