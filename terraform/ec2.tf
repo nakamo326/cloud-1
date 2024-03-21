f4d00efdef7274f343f1502a57bf6e3e185d13fb
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-03-22 00:50:40 +0900

    不要なセキュリティグループのルールを削除

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -20,20 +20,8 @@ resource "aws_security_group" "ec2_sg" {
   name   = "cloud-1-ec2-sg"
   vpc_id = aws_vpc.vpc.id
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
