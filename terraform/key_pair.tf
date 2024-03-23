048eea41e1d3aa96578b906a2662967ac3a750db
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-03-23 17:56:06 +0900

    Revert "一旦variables削除"
    This reverts commit 3a2c1c34c9970f412555d981d1dc8a24af57ab8b.

--- terraform/key_pair.tf
+++ terraform/key_pair.tf
@@ -5,10 +5,10 @@ resource "tls_private_key" "key" {
 }
 
 resource "aws_key_pair" "key" {
-  key_name   = "cloud-1-key-pair"
+  key_name   = var.key_pair_name
   public_key = tls_private_key.key.public_key_openssh
 
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.key.private_key_pem}' > ./${aws_key_pair.key.key_name}.pem && chmod 600 ./${aws_key_pair.key.key_name}.pem"
+    command = "echo '${tls_private_key.key.private_key_pem}' > ./${var.key_pair_name}.pem && chmod 600 ./${var.key_pair_name}.pem"
   }
 }
